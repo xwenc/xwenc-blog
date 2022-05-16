143612c0306a5285b8f71041f5d83d2c22a194e7
thinman918 <627020231@qq.com>
2022-05-16 21:30:39 +0200

    fix style

--- components/tools/tools.js
+++ components/tools/tools.js
@@ -5,9 +5,9 @@ const Tools = ({ tools }) => (
     <h5 className="font-header font-semibold text-front text-sm uppercase mb-3">
       Main skills
     </h5>
-    <ul className="flex flex-wrap pb-8 mt-2 space-x-1">
+    <ul className="flex flex-wrap pb-8 mt-2 -mx-1">
       {tools.map((tool, index) => (
-        <li className="inline-block md:block mb-1 mr-1 md:mr-0" key={index}>
+        <li className="inline-block md:block mb-2 mx-1" key={index}>
           <span className="inline-block subpixel-antialiased bg-lead px-3 py-1 font-header font-light text-xl md:text-2xl text-lead-text">
             {tool.title}
           </span>
